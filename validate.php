e84b75fcd7a4bebdfbcb890fb95c20c880790382
Albert Nazander <alizbazar@alizweb.com>
2014-10-27 00:53:09 +0200

    Mark visited timestamp on every validation

--- validate.php
+++ validate.php
@@ -11,6 +11,10 @@ if (!$invite) {
     $response = array('status' => 'error', 'message' => 'No invitation found', 'code' => 'NOTFOUND');
 } else {
     $response = array('status' => 'success', 'data' => $invite);
+
+    $timestamp = date('Y-m-d G:i:s');
+    $sql = "UPDATE garage_invites SET visited = '$timestamp' WHERE token = '$token'";
+    good_query($sql);
 }
 
 
