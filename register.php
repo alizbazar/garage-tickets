ab362ad7575206fb92ff1ee60d6d6246a3119d9d
Albert Nazander <alizbazar@alizweb.com>
2014-11-15 14:03:21 +0200

    Accept duplicate emails

--- register.php
+++ register.php
@@ -18,17 +18,30 @@ $tokenValid = $db->affected_rows > 0;
 if ($tokenValid) {
     $response = array('status' => 'success');
 } else {
-    $response = array('status' => 'error', 'message' => 'No invitation found', 'code' => 'NOTFOUND');
+    // Check if the error was due to duplicate email -> if so, just update the existing email invite
+    $error = $db->error;
+    if (stripos($error, "Duplicate") !== FALSE && stripos($error, "'email'") !== FALSE) {
+        good_query("UPDATE garage_invites SET name = '$name', phone = '$phone', registered = '$timestamp', visited = '$timestamp' WHERE email = '$email'");
+        if ($db->affected_rows > 0) {
+            $response = array('status' => 'success');
+        } else {
+            $response = array('status' => 'error', 'message' => 'Some unknown error occurred', 'code' => 'UNKNOWN');
+        }
+    } else {
+        $response = array('status' => 'error', 'message' => 'No invitation found', 'code' => 'NOTFOUND');
+    }
 }
 
-$spaceI = strpos($name, ' ', 1);
-if ($spaceI !== FALSE) {
-	$firstName = substr($name, 0, $spaceI);
-} else {
-	$firstName = $name;
-}
+if ($response['status'] == 'success') {
 
-$message = "Hi " . $firstName . "!
+    $spaceI = strpos($name, ' ', 1);
+    if ($spaceI !== FALSE) {
+        $firstName = substr($name, 0, $spaceI);
+    } else {
+        $firstName = $name;
+    }
+
+    $message = "Hi " . $firstName . "!
 
 Thanks for registering for Elisa Garage Chillax that takes place on Tuesday, November 18 from 6-9pm.
 
@@ -46,12 +59,13 @@ PS. Should you have any questions, shoot us an email at info@elisaxslush.com .
 
 ";
 
-//$message = str_replace("\n", "\r\n", $message);
+    //$message = str_replace("\n", "\r\n", $message);
 
-$header = "From: Elisa X Slush <info@elisaxslush.com>\r\n";
-$header .= "Content-Type: text/plain;charset=utf-8\r\n";
+    $header = "From: Elisa X Slush <info@elisaxslush.com>\r\n";
+    $header .= "Content-Type: text/plain;charset=utf-8\r\n";
 
-mail($email, 'Welcome to Elisa Garage Chillax on 18.11.', $message, $header);
+    mail($email, 'Welcome to Elisa Garage Chillax on 18.11.', $message, $header);
+}
 
 respond(json_encode($response));
 
